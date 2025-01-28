ebf9050cc771ef4cde39bd589319c38f4673081e
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-01-28 08:33:39 +0000

    Release 0.0.64

--- src/axiomatic/core/client_wrapper.py
+++ src/axiomatic/core/client_wrapper.py
@@ -16,7 +16,7 @@ class BaseClientWrapper:
         headers: typing.Dict[str, str] = {
             "X-Fern-Language": "Python",
             "X-Fern-SDK-Name": "axiomatic",
-            "X-Fern-SDK-Version": "0.0.63",
+            "X-Fern-SDK-Version": "0.0.64",
         }
         headers["X-API-Key"] = self.api_key
         return headers
